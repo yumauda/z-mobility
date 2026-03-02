f3514d365dada38038ba3e171e3dbd80d1dfc938
yumauda <firststep-yumauda@gmail.com>
2026-03-02 14:05:10 +0900

    Revamp footer design and enhance front page visuals. Updated footer.php with new company information and navigation links, added styles for footer components in styles.css, and integrated SVG logos. Improved GSAP animations for ribbon elements on the front page for better user engagement.

--- footer.php
+++ footer.php
@@ -1,6 +1,70 @@
-<footer class="l-footer p-footer">
+<footer class="p-footer">
     <div class="l-inner">
-        footer.php
+        <div class="p-footer__footer">
+            <div class="p-footer__footerTop">
+                <div class="p-footer__info">
+                    <div class="p-footer__company">
+                        <p class="p-footer__companyName">株式会社Ｚ</p>
+                        <p class="p-footer__companyText">
+                            本社<br>
+                            〒104-0061　東京都中央区銀座8-3-10 トミタビル4F<br>
+                            TEL：050-3733-7504<br>
+                            FAX：050-3733-7505
+                        </p>
+                    </div>
+
+                    <figure class="p-footer__logo">
+                        <img decoding="async" loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/images/common/footer-logo.svg" alt="株式会社Z" width="125" height="116">
+                    </figure>
+                </div>
+                <div class="p-footer__right">
+
+                    <nav class="p-footer__nav" aria-label="フッターナビゲーション">
+                        <div class="p-footer__navCol">
+                            <a class="p-footer__navLink" href="#">Top</a>
+                            <a class="p-footer__navLink" href="#">お知らせ</a>
+                        </div>
+
+                        <div class="p-footer__navCol p-footer__navCol--wide">
+                            <div class="p-footer__navGroup">
+                                <a class="p-footer__navLink" href="#">仕事について</a>
+                                <div class="p-footer__navSub">
+                                    <a class="p-footer__navSublink" href="#">仕事内容</a>
+                                    <a class="p-footer__navSublink" href="#">車両紹介</a>
+                                    <a class="p-footer__navSublink" href="#">二種免許支援教育体制</a>
+                                    <a class="p-footer__navSublink" href="#">数字で見るZ</a>
+                                    <a class="p-footer__navSublink" href="#">よくある質問</a>
+                                </div>
+                            </div>
+                            <a class="p-footer__navLink" href="#">インタビュー動画</a>
+                        </div>
+
+                        <div class="p-footer__navCol">
+                            <div class="p-footer__navGroup">
+                                <a class="p-footer__navLink" href="#">会社情報</a>
+                                <div class="p-footer__navSub">
+                                    <a class="p-footer__navSublink" href="#">代表メッセージ</a>
+                                    <a class="p-footer__navSublink" href="#">会社概要</a>
+                                </div>
+                            </div>
+                            <a class="p-footer__navLink" href="#">募集要項</a>
+                            <a class="p-footer__navLink" href="#">エントリー</a>
+                        </div>
+                    </nav>
+                    <div class="p-footer__footerBottom">
+                        <div class="p-footer__legal">
+                            <a class="p-footer__legalLink" href="#">プライバシーポリシー</a>
+                            <a class="p-footer__legalLink" href="#">運送約款</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        <p class="p-footer__copyright">Copyright © 「Z-MOBILITY」オフィシャルサイト. All rights reserved.</p>
+
     </div>
 </footer>
 <?php wp_footer(); ?>
--- js/gsap.js
+++ js/gsap.js
@@ -1,4 +1,6 @@
-const opening = gsap.timeline()
+gsap.registerPlugin(ScrollTrigger);
+
+const opening = gsap.timeline();
 
 opening.fromTo(".js-mv-img", {
   opacity: 0,
@@ -10,6 +12,15 @@ opening.fromTo(".js-mv-img", {
   ease: "power2.inOut",
   duration: 1.5,
 });
+opening.fromTo(".js-mv-ribbon", {
+  opacity: 0,
+  clipPath: "inset(0 100% 0 0)",
+}, {
+  opacity: 1,
+  clipPath: "inset(0 0% 0 0)",
+  duration: 1.5,
+  ease: 'power2.inOut',
+}, "-=1");
 opening.fromTo(".js-en-title", {
   opacity: 0,
   clipPath: "inset(100% 0 0 0)",
@@ -30,3 +41,29 @@ opening.fromTo(".js-ja-title", {
   duration: 1,
   "--width": "100%",
 }, "-=0.5");
+
+
+
+
+let ribbons = document.querySelectorAll('.js-ribbon');
+
+ribbons.forEach((ribbon) => {
+  gsap.fromTo(
+    ribbon,
+    {
+      opacity: 0,
+      clipPath: "inset(0 100% 0 0)",
+    },
+    {
+      opacity: 1,
+      clipPath: "inset(0 0% 0 0)",
+      duration: 1.5,
+      ease: 'power2.inOut',
+      scrollTrigger: {
+        trigger: ribbon,
+        start: 'top 90%',
+      },
+    }
+  );
+});
+
